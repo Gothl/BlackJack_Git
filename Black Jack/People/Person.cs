c40595d7bf897db5a65d537ab0dbdc23c9022226
Gothl <gothlip@hotmail.com>
2022-02-28 13:12:32 +0100

    Inlined all int fields. + changed comments wrt. new cardList format + added summaries

--- Black Jack/People/Person.cs
+++ Black Jack/People/Person.cs
@@ -2,22 +2,25 @@ namespace Black_Jack {
     public abstract class Person {
         //public int id;
         public string? name;
-        public int bankroll, bet;
+        public int bankroll, bet, roundWinnings;
         public bool isActive = true;//New people are active by default.
-        public Hand hand = new Hand();//New empty CardArray for the person's hand of cards.
+        public Hand hand = new Hand();//New empty CardList for the person's hand of cards.
 
+
+        /// <summary> Declares the person bust, if the hand total is <= 21. </summary>
         public void Bust() {
             if (hand.total <= 21){
                 System.Console.WriteLine("{0} is bust, and has lost this round.", name);
                 isActive = false;
-                
             }
-            }//TODO: implement different rules for what happens for different people (possibly using an overwrite in the dealer's case)
+        }//TODO: implement different rules for what happens for different people (possibly using an overwrite in the dealer's case)
         
+
         /// <summary> Deposit gamling credits. </summary>
         public void DepositToBankroll(int credits){bankroll += credits;}
 
-        /// <summary> Deposit gamling credits. </summary>
+
+        /// <summary> Withdraw gamling credits. </summary>
         public void WithdrawFromBankroll(int credits){//TODO: Check that that amount is available before calling function.
             // System.Console.WriteLine("Please enter the amount of credits, you wish to withdraw.");
             // System.Console.ReadLine(); 
