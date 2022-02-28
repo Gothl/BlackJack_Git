2e48326d13c8bfca1c14d08b23450cd7750d5362
Gothl <gothlip@hotmail.com>
2022-02-28 13:14:05 +0100

    Added preprocessor variable. + summary + functionality to DealCards()

--- Black Jack/People/Dealer.cs
+++ Black Jack/People/Dealer.cs
@@ -1,10 +1,27 @@
+#define DEALER_TEST
 namespace Black_Jack {
     public class Dealer : Person {
-        public Dealer(){name="The Dealer";}
+        public Dealer(){
+            #if DEALER_TEST
+                System.Console.WriteLine("DEALER: A new Dealer i being created");
+            #endif
+                name="the dealer";
+        }
         public int faceDownCardValue;
-        public void DealCards(int n, int playerID){
-            //Take card from deck
-            
+
+
+        /// <summary> Deal cards from deck to player. </summary>
+        public void DealCards(Deck d, int n, Person p){//TODO: figure out if you want to use the n here.
+            //Take card from deck (move it from Deck to Person P's hand. )
+            #if DEALER_TEST
+                System.Console.WriteLine("DEALER: Dealer.DealCards is entered");
+            #endif
+            p.hand.cardList.Add(d.cardList[0]);
+            d.cardList.Remove(d.cardList[0]);
+
+            #if DEALER_TEST
+                System.Console.WriteLine("DEALER: {0} now has {1} cards in their hand, and there are {2} cards left in the deck.", p.name,  p.hand.cardList.Count, d.cardList.Count);
+            #endif
             //
         }
     }
