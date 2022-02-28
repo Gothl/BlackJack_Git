bceaef86efe40446b56a4e68eef909e0a955d557
Gothl <gothlip@hotmail.com>
2022-02-28 16:20:09 +0100

    Added the obligatory, but in this case unimple- mented CheckHand method

--- Black Jack/People/NPC.cs
+++ Black Jack/People/NPC.cs
@@ -2,5 +2,6 @@ namespace Black_Jack {
     public class NPC : Person {
         //might use this class in later implementation
         public NPC(){name="NPC";}//TODO: get better solution for numbering of NPC, in case there is more than one.
+        public override HandState CheckHand(){throw new NotImplementedException();}
     }
 }
