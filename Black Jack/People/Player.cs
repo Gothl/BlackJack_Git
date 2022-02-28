3cae380ce7b39819d550fd48527a08cd141b62ab
Gothl <gothlip@hotmail.com>
2022-02-28 13:11:20 +0100

    Added preprocessor variable. + some comments/outline for ChooseAction()

--- Black Jack/People/Player.cs
+++ Black Jack/People/Player.cs
@@ -1,10 +1,28 @@
+#define PLAYER_TEST
 namespace Black_Jack {
     public class Player : Person {
         public int Bet {get; set;}
-        public Player(string nm){name = nm;}//TODO: also set bet at some point.
-        public void ChooseAction(string a){}
+        public Player(string nm){
+            #if PLAYER_TEST
+                System.Console.WriteLine("PLAYER: A new player is being created");
+            #endif 
+            name = nm;
+        }//TODO: also set bet at some point.
+
+
+        /// <summary> Choose to hit or stay. </summary>
+        public void ChooseAction(string a){
+            //Ask for choice of action:
+
+            //if "hit":
+
+            //if "stay"
+
+            //else (invalid input)
+        }
 
         /// <summary> Place bet for that round. </summary>
-        public void PlaceBet(int credits){Bet += credits;}
+        public void PlaceBet(int credits){    
+            Bet += credits;}
     }
 }
