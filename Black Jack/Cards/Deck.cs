2623935d128e0b42ccfe4649e1369fb55f81ca48
Gothl <gothlip@hotmail.com>
2022-02-28 13:06:47 +0100

    Implemented the change to cardLists instead of arrays. + added Preprocessor variable

--- Black Jack/Cards/Deck.cs
+++ Black Jack/Cards/Deck.cs
@@ -1,41 +1,44 @@
+#define DECK_TEST
 namespace Black_Jack {
-    public class Deck : CardArray {
+    public class Deck : CardList {
         public bool isEmpty;
         public Deck(){
+            #if DECK_TEST
+                System.Console.WriteLine("A new deck is being created");
+            #endif
             // Create array of 24 copies of numbers between 1 and 9.
-            //int[] arr = {};
             for (var j = 1; j < 10; ++j){
                 for (var i = 0; i < 24; ++i){
-                    arr = arr.Append(j).ToArray();
+                    cardList.Add(j);
                 }
             }
             //Add the 4*4*6=96 copies of 10 (for the cards 10, J, Q and K) to the array.
-            arr = arr.Concat(Enumerable.Repeat(10, 96)).ToArray();
+            cardList = cardList.Concat(Enumerable.Repeat(10, 96)).ToList();
 
-
-            // Testprint of all numbers/cards
-            // Array.ForEach(arr, Console.WriteLine);
-            // Testcount of all cards.
-            // Console.WriteLine("the array has {0} elements",arr.Length);
-            
-            // for (var i = 1; i < arr.Length; i++)
-            // arr[i] = i;
+            #if DECK_TEST
+                Console.WriteLine("the new deck has {0} elements", cardList.Count);
+            #endif
 
             //Shuffle Deck
             Random random = new Random();//TODO: possibly get better randomisation solution.
-            arr = arr.OrderBy(x => random.Next()).ToArray();
+            cardList = cardList.OrderBy(x => random.Next()).ToList();
+
+            #if DECK_TEST
+            Console.WriteLine("\n Now all {0} numbers in random order:\n", cardList.Count);
+                foreach (var i in cardList){Console.WriteLine(i);}
+            #endif
         }    
             
-        // Console.WriteLine("\n Now all {0} numbers in random order:\n", arr.Length);
-            // foreach (var i in arr)
+        // Console.WriteLine("\n Now all {0} numbers in random order:\n", cardList.Count);
+            // foreach (var i in cardList)
             // {
             //     Console.WriteLine(i);
             // }
             // //Shuffling the deck a second time:
-            // arr = arr.OrderBy(x => random.Next()).ToArray();
+            // cardList.OrderBy(x => random.Next());
             
-            // Console.WriteLine("\n All {0} numbers after second shuffling:\n", arr.Length);
-            // foreach (var i in arr)
+            // Console.WriteLine("\n All {0} numbers after second shuffling:\n", cardList.Count);
+            // foreach (var i in cardList)
             // {
             //     Console.WriteLine(i);
             // }
