eac499e58f0badcd9638447c7dabb55bca88dadb
Gothl <gothlip@hotmail.com>
2022-02-28 16:16:13 +0100

    Added HandState enum

---
+++ Black Jack/HandState.cs
@@ -0,0 +1,11 @@
+namespace Black_Jack{
+    public enum HandState{
+        Dealer_Above21,
+        Dealer_Equals21,
+        Dealer_Below17,
+        Dealer_Below21,
+        Player_Above21,
+        Player_Equals21,
+        Player_Below21
+    }
+}
