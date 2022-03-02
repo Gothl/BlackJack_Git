467c14bef884cd098d120e8b56265ad641691cb5
Gothl <gothlip@hotmail.com>
2022-03-02 15:15:55 +0100

    Added better parsing

--- Black Jack/GamePlay/Game.cs
+++ Black Jack/GamePlay/Game.cs
@@ -28,25 +28,16 @@ namespace Black_Jack {
             #endif
             System.Console.WriteLine("To play, you need to deposit an amount of credits to your bankroll, to be used for betting.");
             System.Console.WriteLine("Please enter an amount of whole numbers between 1.000 and 50.000, excluding dots.");
+
+
             string? depositString = Console.ReadLine();
-            int deposit = 0;
-            while (true){
-                
-                if (String.IsNullOrWhiteSpace(depositString)){
-                    System.Console.WriteLine("Invalid input. Please enter an integer between 1000 and 50000."); 
-                    depositString = Console.ReadLine();
-                    continue;
-                }
-                //TODO: Add check for other data types than int.
-                deposit = Convert.ToInt32(depositString);
-                if (deposit < 1000  || deposit > 50000){ //while deposit out of specified bounds.
-                    System.Console.WriteLine("Invalid number. Please enter an integer between 1000 and 50000.");//TODO: ask for deposit again
-                    depositString = Console.ReadLine();
-                    continue;
-                }
-                break;//input is a string representation of a whole number between 1000 ad 50000
-            }
-            peopleInGame[0].DepositToBankroll(deposit);
+            int deposit;
+            while (!int.TryParse(depositString, out deposit) || (deposit < 1000  || deposit > 50000)){
+                System.Console.WriteLine("Invalid input. Please enter an integer between 1000 and 50000."); 
+                depositString = Console.ReadLine();
+            }    
+
+            peopleInGame[1].DepositToBankroll(deposit);
             System.Console.WriteLine("\nThank you! A deposit of {0} credits has been made to your bankroll,\nwhich you can now use to place your bet for the first round.", deposit);
             System.Console.WriteLine("\nThe game can now begin. You will be playing against the dealer.");
         }
@@ -59,17 +50,29 @@ namespace Black_Jack {
             bool beginNewRound = true;
             while (beginNewRound) {
                 //Starting a new round
-                Round r = new Round();
+                Round r = new Round(peopleInGame);
                 //#if GAME_TEST
                 //    System.Console.WriteLine("TEST: The deck is now generated:"); r.d.TEST_PrintDeck();
                 //#endif
                 //after the end of a round:
                 // CHECK if new round is wanted.
-                System.Console.WriteLine("Do you want to play another round?");
+                System.Console.WriteLine("Do you want to play another round? Enter Y/y for \"yes\" and N/n for \"No\".");
                 string? answer = System.Console.ReadLine();
-                foreach (Person p in r.peopleInRound){ // <-- possibly move this to round class.
-                    if (!p.isActive) {r.RemovePersonFromRound(p);}
-                }
+                if (answer == "Y" || answer == "y"){continue;}
+                if (answer == "N" || answer == "n"){
+                    beginNewRound = false;
+                    System.Console.WriteLine("Thank you for playing.");
+                    }
+                else {
+                    beginNewRound = false;
+                    System.Console.WriteLine("Invalid input. There is no functionality defined for this.\nThe game will now end.");
+                    }
+                
+                System.Console.WriteLine("The bankroll of each player is as follows:");
+                    foreach (Person p in peopleInGame){
+                        System.Console.WriteLine("{0}: {1} credits", p.name, p.bankroll);
+                    }
+                System.Console.WriteLine("Goodbye.");
             }
         }
         
