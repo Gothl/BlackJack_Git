870d80c1b064b409b419eeb58fb406643793c762
Gothl <gothlip@hotmail.com>
2022-03-02 15:18:58 +0100

    Added isDealer field + empty ChooseAction()

--- Black Jack/People/NPC.cs
+++ Black Jack/People/NPC.cs
@@ -1,7 +1,10 @@
 namespace Black_Jack {
     public class NPC : Person {
         //might use this class in later implementation
-        public NPC(){name="NPC";}//TODO: get better solution for numbering of NPC, in case there is more than one.
+        public NPC(){name="NPC"; isDealer = false;}//TODO: get better solution for numbering of NPC, in case there is more than one.
         public override HandState CheckHand(){throw new NotImplementedException();}
+
+        public  override ActionState ChooseAction(){throw new NotImplementedException();}
+
     }
 }
