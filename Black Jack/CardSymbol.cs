4cab8bbfec51a2ed69305d1e18626b98d4132708
Gothl <gothlip@hotmail.com>
2022-03-03 14:08:03 +0100

    Implemented CardSymbol Enum

---
+++ Black Jack/CardSymbol.cs
@@ -0,0 +1,18 @@
+namespace Black_Jack{
+    public enum CardSymbol{
+        Joker,//Acts as buffer so no need to explicitly specify value of cards Aces through 10.
+        Aces,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
