41207d5baede223fdd9e07c6ed0603e4ecc2b83c
Gothl <gothlip@hotmail.com>
2022-03-04 06:30:34 +0100

    Added calculation of total

--- Black Jack/Cards/Hand.cs
+++ Black Jack/Cards/Hand.cs
@@ -2,8 +2,13 @@ namespace Black_Jack {
     public class Hand : CardList {
         public int total;
         public HandState status;
-        public int Total {
-            get {return cardList.Count;}
+        public void CalculateTotal() {
+            total = 0;
+            foreach (Card c in this.cardList){
+                //System.Console.WriteLine("Cardvalue is {0}", c.cardValue);
+                total += c.cardValue;
+                //System.Console.WriteLine("Total is now {0}", total);
+            }
         }
                 
     }
